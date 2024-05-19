572a344e2b4817513ce7dd124dffd1cd2d427191
Hüsne KÖMESÖĞÜTLÜ <96977741+HusneSakalli@users.noreply.github.com>
2024-05-19 23:25:05 +0300

    Add files via upload
    Güncellendi...

--- ilk_odev.dart
+++ ilk_odev.dart
@@ -4,5 +4,7 @@ void main() {
   String yas='24 yaşında,';
   String resit='reşitlik durumu=';
   bool resitlikDurumu=true;
-  print('$ad $soyAd $yas $resit $resitlikDurumu');
+int money=10;
+
+  print('adım :$ad soyadım : $soyAd $yas $resit $resitlikDurumu Para durumu $money');
 }
