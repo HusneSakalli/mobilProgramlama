4974a9f7976864f91ed4f9f250119c58133a38dd
Hüsne KÖMESÖĞÜTLÜ <96977741+HusneSakalli@users.noreply.github.com>
2024-05-19 22:59:54 +0300

    Add files via upload

---
+++ ilk_odev.dart
@@ -0,0 +1,8 @@
+void main() {
+  String ad='Hüsne';
+  String soyAd='Kömesöğütlü';
+  String yas='24 yaşında,';
+  String resit='reşitlik durumu=';
+  bool resitlikDurumu=true;
+  print('$ad $soyAd $yas $resit $resitlikDurumu');
+}
